6fcc844769e9a0c5faafaff3a6fc716590231311
unknown <aril.imata061103@gmail.com>
2023-09-21 08:20:39 +0700

    First Commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+// // Sticky Header
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var header = document.getElementById("header-container");
+
+// // Get the offset position of the navbar
+// var fixedPosition = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.scrollY > fixedPosition) {
+//       header.classList.add("fixed-position");
+//     } else {
+//       header.classList.remove("fixed-position");
+//     }
+// }
